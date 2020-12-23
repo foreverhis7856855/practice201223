9c52270260a0f1e7f81fbfd0b08e32e9388a741c
foreverhis7856 <foreverhis@naver.com>
2020-12-23 22:08:12 +0900

    파이썬 헬로우월드 연습

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+Python 3.7.5 (default, Nov  7 2019, 10:50:52) 
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello, world!')
+Hello, world!
+>>> 
